39c250ea3a3748e16edab1b913a01ffb4e948d92
User <User@DESKTOP-SBGCVVH>
2025-12-06 06:05:35 +0800

    more latest

--- FinalProject/RecentTransaction.Designer.cs
+++ FinalProject/RecentTransaction.Designer.cs
@@ -31,11 +31,6 @@
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
             dgViewUsers = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -45,17 +40,17 @@
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.Black;
-            pictureBox1.Location = new Point(-8, 210);
+            pictureBox1.Location = new Point(6, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(402, 449);
+            pictureBox1.Size = new Size(808, 449);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(-8, 236);
+            pictureBox2.Location = new Point(6, 38);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(402, 394);
+            pictureBox2.Size = new Size(808, 394);
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
             // 
@@ -64,73 +59,18 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Black;
             label1.ForeColor = Color.White;
-            label1.Location = new Point(12, 218);
+            label1.Location = new Point(26, 20);
             label1.Name = "label1";
             label1.Size = new Size(107, 15);
             label1.TabIndex = 2;
             label1.Text = "Recent Transaction";
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = SystemColors.Control;
-            label2.ForeColor = Color.Black;
-            label2.Location = new Point(12, 273);
-            label2.Name = "label2";
-            label2.Size = new Size(107, 15);
-            label2.TabIndex = 3;
-            label2.Text = "Recent Transaction";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = SystemColors.Control;
-            label3.ForeColor = Color.Black;
-            label3.Location = new Point(12, 352);
-            label3.Name = "label3";
-            label3.Size = new Size(107, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Recent Transaction";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.BackColor = SystemColors.Control;
-            label4.ForeColor = Color.Black;
-            label4.Location = new Point(12, 494);
-            label4.Name = "label4";
-            label4.Size = new Size(107, 15);
-            label4.TabIndex = 6;
-            label4.Text = "Recent Transaction";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.BackColor = SystemColors.Control;
-            label5.ForeColor = Color.Black;
-            label5.Location = new Point(12, 427);
-            label5.Name = "label5";
-            label5.Size = new Size(107, 15);
-            label5.TabIndex = 5;
-            label5.Text = "Recent Transaction";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.BackColor = SystemColors.Control;
-            label6.ForeColor = Color.Black;
-            label6.Location = new Point(12, 567);
-            label6.Name = "label6";
-            label6.Size = new Size(107, 15);
-            label6.TabIndex = 7;
-            label6.Text = "Recent Transaction";
-            // 
             // dgViewUsers
             // 
             dgViewUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgViewUsers.Location = new Point(136, 262);
+            dgViewUsers.Location = new Point(26, 60);
             dgViewUsers.Name = "dgViewUsers";
-            dgViewUsers.Size = new Size(240, 344);
+            dgViewUsers.Size = new Size(776, 344);
             dgViewUsers.TabIndex = 8;
             dgViewUsers.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -138,19 +78,13 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(388, 653);
+            ClientSize = new Size(826, 485);
             Controls.Add(dgViewUsers);
-            Controls.Add(label6);
-            Controls.Add(label4);
-            Controls.Add(label5);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Name = "RecentTransaction";
             Text = "RecentTransaction";
-            //Load += this.RecentTransaction_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)dgViewUsers).EndInit();
@@ -163,11 +97,6 @@
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
         private DataGridView dgViewUsers;
     }
 }
